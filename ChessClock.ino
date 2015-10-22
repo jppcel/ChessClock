3b1abca347f7107990eb52f66861821ce4b68028
João Paulo Polles <joaopaulopolles@gmail.com>
2015-10-22 04:15:55 -0200

    Agora já está começando a contar o tempo, porém, há necessidades de implementação ainda.

--- ChessClock.ino
+++ ChessClock.ino
@@ -29,6 +29,39 @@ void setup()
   pinMode(12, INPUT);// return
 }
 
+
+
+  int trying = true;
+
+
+
+  int a1 = 1;
+  int a2 = 1;
+  int btn = 0;
+  int count = 0;
+  char tempString[10] = "1-SA"; //Used for sprintf
+  int et2 = 0;
+  int et3 = 0;
+
+  int btn1 = 9;
+  int btn2 = 10;
+  int btn3 = 11;
+  int btn4 = 12;
+
+  
+  int btnNegras = 12;
+
+  int numStep = 1;
+  int programSeconds = 0;
+  int programAdd = 0;
+  int programAddSeconds = 0;
+  boolean Step1 = true;
+  boolean Step2 = false;
+  boolean Step3 = false;
+
+
+  boolean Introduction = true;
+
 void setCar(byte dig7, byte dig6, byte dig5, byte dig4, byte dig3, byte dig2, byte dig1, byte dig0){
  if(dig7 != null){
   lc.setRow(0,7,dig7);
@@ -56,104 +89,8 @@ void setCar(byte dig7, byte dig6, byte dig5, byte dig4, byte dig3, byte dig2, by
  }
 }
 
-/*void setWord(char Word[9], char Point[9], int num){
-
-  
-  int dig = 0;
-  int local = 7;
-  int Max = 7;
-  int dp;
-  int count = 0;
-  char digit;
-
-  
-  for(int i = Max; i >= 0; i--){
-    digit = Word[i];
-
-    if(Point[i] == '.'){
-      dp = 1;
-    }else{
-      dp = 0;
-    }
-       
-    if(count >= num){
-      dp=1;
-    }
-    count++;
-  
-    byte seg7;
-    if(digit != -1 || count < num){
-      switch(digit){
-        case 'A':
-        case 'a':
-          if(dp == 1){ seg7 = B11110111; } else { seg7 = B01110111; }
-          break;
-        case 'B':
-        case 'b':
-          if(dp == 1){ seg7 = B10011111; } else { seg7 = B00011111; }
-          break;
-        case 'C':
-        case 'c':
-          if(dp == 1){ seg7 = B11001110; } else { seg7 = B01001110; }
-          break;
-        case 'D':
-        case 'd':
-          if(dp == 1){ seg7 = B10111101; } else { seg7 = B00111101; }
-          break;
-        case 'E':
-        case 'e':
-          if(dp == 1){ seg7 = B11001111; } else { seg7 = B01001111; }
-          break;
-        case 'F':
-        case 'f':
-          if(dp == 1){ seg7 = B11000111; } else { seg7 = B01000111; }
-          break;
-        case 'G':
-        case 'g':
-        case '6':
-          if(dp == 1){ seg7 = B11011111; } else { seg7 = B01011111; }
-          break;
-        case 'H':
-        case 'h':
-          if(dp == 1){ seg7 = B10110111; } else { seg7 = B00110111; }
-          break;
-        case 'I':
-        case 'i':
-          if(dp == 1){ seg7 = B10010000; } else { seg7 = B00010000; }
-          break;
-        case 'J':
-        case 'j':
-          if(dp == 1){ seg7 = B10111000; } else { seg7 = B00111000; }
-          break;
-        case 'L':
-        case 'l':
-          if(dp == 1){ seg7 = B10001110; } else { seg7 = B00001110; }
-          break;
-      }
-    }else{
-      seg7 = B00000000;
-    }
-  
-    /*byte dis = seg7;
-          
-    if(dp == '.'){
-      lc.setRow(0,local,concatByte('B1',seg7));
-    }else{
-      lc.setRow(0,local,concatByte('B0',seg7));
-    }
-  
-  
-  
-    lc.setRow(0, dig, seg7);
-    Serial.println(dig+" - "+digit);
-  }
-  local--;
-  dig++;
-  count++;
-}*/
-
 char ToShow[2][4];
-char Toshow[8];
+int Toshow[8];
 void setDig(char Word[8], char Point[8], int num){
 
 
@@ -165,15 +102,16 @@ void setDig(char Word[8], char Point[8], int num){
   int count = 0;
   char digit;
 
-  if(Word == "*ToShow*"){
-    //sprintf(Word, "%s%s", ToShow[0], ToShow[1]); 
-    Word = Toshow;
-  }
-  Serial.println(ToShow[0]);
+  Serial.println(Word);
 
   
   for(int i = Max; i >= 0; i--){
-    digit = Word[i];
+    
+    /*if(Word == "*ToShow*"){
+      int digit = Toshow[i];
+    }else{*/
+      char digit = Word[i];
+    //}
 
     if(Point[i] == '.'){
       dp = 1;
@@ -227,33 +165,83 @@ void setDig(char Word[8], char Point[8], int num){
   }
 }
 
-  int trying = true;
+void setNum(char Point[8], int num){
 
 
-
-  int a1 = 1;
-  int a2 = 1;
-  int btn = 0;
+  
+  int dig = 0;
+  int local = 7;
+  int Max = 7;
+  int dp;
   int count = 0;
-  char tempString[10] = "1-SA"; //Used for sprintf
-  int et2 = 0;
-  int et3 = 0;
+  int digit;
 
-  int btn1 = 9;
-  int btn2 = 10;
-  int btn3 = 11;
-  int btn4 = 12;
-
-  int numStep = 1;
-  int programSeconds = 0;
-  int programAdd = 0;
-  int programAddSeconds = 0;
-  boolean Step1 = true;
-  boolean Step2 = false;
-  boolean Step3 = false;
+  /*for(int j = 0; j < 8; j++){
+    if(j == 7){
+      Serial.println(Toshow[j]);
+    }else{
+      Serial.print(Toshow[j]);
+    }
+  }*/
+  Serial.println(programSeconds);
 
 
-  boolean Introduction = true;
+  
+  for(int i = Max; i >= 0; i--){
+    
+    int digit = Toshow[i];
+
+    if(Point[i] == '.'){
+      dp = 1;
+    }else{
+      dp = 0;
+    }
+    if(Point[i] != '*'){
+      if(digit == 0){
+        if(dp == 1){ lc.setRow(0, dig, B11111110); } else { lc.setRow(0, dig, B01111110); }
+      }
+      if(digit == 1){
+        if(dp == 1){ lc.setRow(0, dig, B10110000); } else { lc.setRow(0, dig, B00110000); }
+      }
+      if(digit == 2){
+        if(dp == 1){ lc.setRow(0, dig, B11101101); } else { lc.setRow(0, dig, B01101101); }
+      }
+      if(digit == 3){
+        if(dp == 1){ lc.setRow(0, dig, B11111001); } else { lc.setRow(0, dig, B01111001); }
+      }
+      if(digit == 4){
+        if(dp == 1){ lc.setRow(0, dig, B10110011); } else { lc.setRow(0, dig, B00110011); }
+      }
+      if(digit == 5){
+        if(dp == 1){ lc.setRow(0, dig, B11011011); } else { lc.setRow(0, dig, B01011011); }
+      }
+      if(digit == 6){
+        if(dp == 1){ lc.setRow(0, dig, B11011111); } else { lc.setRow(0, dig, B01011111); }
+      }
+      if(digit == 7){
+        if(dp == 1){ lc.setRow(0, dig, B11110000); } else { lc.setRow(0, dig, B01110000); }
+      }
+      if(digit == 8){
+        if(dp == 1){ lc.setRow(0, dig, B11111111); } else { lc.setRow(0, dig, B01111111); }
+      }
+      if(digit == 9){
+        if(dp == 1){ lc.setRow(0, dig, B11111011); } else { lc.setRow(0, dig, B01111011); }
+      }
+      if(digit == 9){
+        if(dp == 1){ lc.setRow(0, dig, B11111011); } else { lc.setRow(0, dig, B01111011); }
+      }
+      if(Point[i] == ','){
+        lc.setRow(0, dig, B10000000);
+      }
+    }else{
+        lc.setRow(0, dig, B00000000);
+    }
+  
+    local--;
+    dig++;
+    count++;
+  }
+}
 
 void returnStep(){
   if(numStep == 3){
@@ -279,59 +267,178 @@ int toSeconds(int hours, int minutes, int seconds){
   return total;
 }
 
+int toMiliseconds(int hours, int minutes, int seconds){
+  int total = seconds;
+  total = total + (minutes*60);
+  total = total + (hours*60*60);
+  return total*10;
+}
+
 
 void toShow(int seconds1, int seconds2){
     int hours, secHours, minutes, Seconds;
+    int H[2], SH[2], M[2], S[2];
     unsigned char seconds[2];
-    seconds[0] = seconds1;
-    seconds[1] = seconds2;
+    seconds[0] = seconds1/10;
+    seconds[1] = seconds2/10;
+
+    
   for(int i=0; i<2; i++){
     hours = (int)(seconds[i]/60)/60;
-    secHours = hours*60*60;
-    minutes = (int)(seconds1-(secHours))/60;
-    Seconds = seconds[i] - secHours - (minutes*60);
-    if(hours > 0){
-      if(hours >= 10){
-        if(minutes >= 10){
-          sprintf(ToShow[i], "%d%d", hours, minutes);
-        }else{
-          if(minutes == 0){
-            sprintf(ToShow[i], "%d%d%d", hours, 0, 0);
-          }else{
-            sprintf(ToShow[i], "%d%d%d", hours, 0, minutes);
-          }
-        }
-      }else{
-        if(minutes >= 10){
-          sprintf(ToShow[i], "%d%d%d", 0, hours, minutes);
-        }else{
-          if(minutes == 0){
-            sprintf(ToShow[i], "%d%d%d%d", 0, hours, 0, 0);
-          }else{
-            sprintf(ToShow[i], "%d%d%d%d", 0, hours, 0, minutes);
-          }
-        }
-      }
+    
+    //Caracters on the arrays
+    if(hours == 0){
+      H[0] = 0;
+      H[1] = 0;
     }else{
-      if(minutes >= 10){
-        if(Seconds >= 10){
-          sprintf(ToShow[i], "%d%d", minutes, Seconds);
-        }else{
-          sprintf(ToShow[i], "%d%d%d", minutes, 0, Seconds);
-        }
+      if(hours >= 10){
+        H[0] = (int) hours/10;
+        H[1] = hours-H[0];
       }else{
-        if(Seconds >= 10){
-          sprintf(ToShow[i], "%d%d%d", 0, minutes, Seconds);
-        }else{
-          if(Seconds == 0){
-            sprintf(ToShow[i], "%d%d%d%d", 0,  minutes, 0, 0);
-          }else{
-            sprintf(ToShow[i], "%d%d%d%d", 0,  minutes, 0, Seconds);
-          }
-        }
+      H[0] = 0;
+      H[1] = hours;
       }
     }
-    sprintf(Toshow, "%s%s", ToShow[0], ToShow[1]);
+    
+    secHours = hours*60*60;
+    minutes = (int)(seconds1-(secHours))/60;
+    
+    //Caracters on the arrays
+    if(minutes == 0){
+      M[0] = 0;
+      M[1] = 0;
+    }else{
+      if(minutes >= 10){
+        M[0] = (int) minutes/10;
+        M[1] = minutes - M[0];
+      }else{
+        M[0] = 0;
+        M[1] = minutes;
+      }
+    }
+    
+    Seconds = seconds[i] - secHours - (minutes*60);
+    
+    //Caracters on the arrays
+    if(seconds == 0){
+      S[0] = 0;
+      S[1] = 0;
+    }else{
+      if(Seconds >= 10){
+        S[0] = (int) Seconds/10;
+        S[1] = Seconds - S[0];
+      }else{
+        S[0] = 0;
+        S[1] = Seconds;
+      }
+    }
+
+    
+    int add;
+    if(i == 0){
+      add = 0;
+    }else{
+      add = 4;
+    }
+
+    if(H[1] >= 1){
+      Toshow[0+add] = H[0];
+      Toshow[1+add] = H[1];
+      Toshow[2+add] = M[0];
+      Toshow[3+add] = M[1];
+    }else{
+      Toshow[0+add] = M[0];
+      Toshow[1+add] = M[1];
+      Toshow[2+add] = S[0];
+      Toshow[3+add] = S[1];
+    }
+    
+    
+  }
+}
+
+
+
+
+void toDisplayTime(int seconds1, int seconds2){
+    int hours, secHours, minutes, Seconds;
+    int H[2], SH[2], M[2], S[2], seconds[2];
+
+    seconds[0] = seconds1/10;
+    seconds[1] = seconds2/10;
+
+    
+  for(int i=0; i<2; i++){
+    hours = (int)(seconds[i]/60)/60;
+    
+    //Caracters on the arrays
+    if(hours == 0){
+      H[0] = 0;
+      H[1] = 0;
+    }else{
+      if(hours >= 10){
+        H[0] = (int) hours/10;
+        H[1] = hours-(H[0]*10);
+      }else{
+        H[0] = 0;
+        H[1] = hours;
+      }
+    }
+    
+    secHours = hours*60*60;
+    minutes = (int)(seconds[i]-secHours)/60;
+    
+    //Caracters on the arrays
+    if(minutes == 0){
+      M[0] = 0;
+      M[1] = 0;
+    }else{
+      if(minutes >= 10){
+        M[0] = (int) minutes/10;
+        M[1] = minutes - (M[0]*10);
+      }else{
+        M[0] = 0;
+        M[1] = minutes;
+      }
+    }
+    
+    Seconds = seconds[i] - secHours - (minutes*60);
+    
+    //Caracters on the arrays
+    if(seconds == 0){
+      S[0] = 0;
+      S[1] = 0;
+    }else{
+      if(Seconds >= 10){
+        S[0] = (int) Seconds/10;
+        S[1] = Seconds - (S[0]*10);
+      }else{
+        S[0] = 0;
+        S[1] = Seconds;
+      }
+    }
+
+    
+    int add;
+    if(i == 0){
+      add = 0;
+    }else{
+      add = 4;
+    }
+
+    if(H[1] >= 1){
+      Toshow[0+add] = H[0];
+      Toshow[1+add] = H[1];
+      Toshow[2+add] = M[0];
+      Toshow[3+add] = M[1];
+    }else{
+      Toshow[0+add] = M[0];
+      Toshow[1+add] = M[1];
+      Toshow[2+add] = S[0];
+      Toshow[3+add] = S[1];
+    }
+    
+    
   }
 }
 
@@ -386,14 +493,9 @@ void introduction(){
     lc.setRow(0, 6, B10000001);
     lc.setRow(0, 7, B10000001);
     delay(300);
-    lc.setRow(0, 0, B00000000);
-    lc.setRow(0, 1, B00000000);
-    lc.setRow(0, 2, B00000000);
-    lc.setRow(0, 3, B00000000);
-    lc.setRow(0, 4, B00000000);
-    lc.setRow(0, 5, B00000000);
-    lc.setRow(0, 6, B00000000);
-    lc.setRow(0, 7, B00000000);
+
+    lc.clearDisplay(0);
+
     Introduction = false;
   }
 }
@@ -420,7 +522,7 @@ void step1(){
         trying = true;
         btn = 0;
         a2 = 1;
-        numStep = 2; setDig("0002", "***0,,,,", 8);
+        numStep = 2;
       }
     }else{
       switch(a1){
@@ -443,11 +545,12 @@ void step2(){
   while(Step2){
     if(a1 == 1){
       while(trying){
+        lc.clearDisplay(0);
         setDig("00030000", "0.00....", 8);
         trying = false;
         delay(400);
         setDig("00030000", "0.000000", 8);
-        programSeconds = toSeconds(0,3,0);
+        programSeconds = toMiliseconds(0,3,0);
         programAdd = 0;
         programAddSeconds = 0;
       }
@@ -487,43 +590,43 @@ void step2(){
           switch(a2){
             case 1:  
               setDig("00030000", "0.000000", 8);
-              programSeconds = toSeconds(0,3,0);
+              programSeconds = toMiliseconds(0,3,0);
               programAdd = 0;
               programAddSeconds = 0;
               break;
             case 2:  
               setDig("00050000", "0.000000", 8);
-              programSeconds = toSeconds(0,5,0);
+              programSeconds = toMiliseconds(0,5,0);
               programAdd = 0;
               programAddSeconds = 0;
               break;
             case 3:  
               setDig("00100000", "0.000000", 8);
-              programSeconds = toSeconds(0,10,0);
+              programSeconds = toMiliseconds(0,10,0);
               programAdd = 0;
               programAddSeconds = 0;
               break;
             case 4:  
               setDig("00150000", "0.000000", 8);
-              programSeconds = toSeconds(0,15,0);
+              programSeconds = toMiliseconds(0,15,0);
               programAdd = 0;
               programAddSeconds = 0;
               break;
             case 5:  
               setDig("00200000", "0.000000", 8); 
-              programSeconds = toSeconds(0,20,0);
+              programSeconds = toMiliseconds(0,20,0);
               programAdd = 0;
               programAddSeconds = 0;
               break;
             case 6:  
               setDig("01300000", "0.000000", 8);
-              programSeconds = toSeconds(1,30,0);
+              programSeconds = toMiliseconds(1,30,0);
               programAdd = 0;
               programAddSeconds = 0;
               break;
             case 7:  
               setDig("02000000", "0.000000", 8);
-              programSeconds = toSeconds(2,0,0);
+              programSeconds = toMiliseconds(2,0,0);
               programAdd = 0;
               programAddSeconds = 0;
               break;
@@ -542,7 +645,7 @@ void step2(){
         trying = false;
         delay(400);
         setDig("00030002", "0.000.00", 8);
-        programSeconds = toSeconds(0,3,0);
+        programSeconds = toMiliseconds(0,3,0);
         programAdd = 1;
         programAddSeconds = 2;
       }
@@ -568,6 +671,7 @@ void step2(){
             Step3 = true;
             trying = true;
             btn = 0;
+      trying = true;
             numStep = 3;
           }else{
             Step2 = false;
@@ -582,19 +686,19 @@ void step2(){
           switch(a2){
             case 1:  
               setDig("00030002", "0.000.00", 8);
-              programSeconds = toSeconds(0,3,0);
+              programSeconds = toMiliseconds(0,3,0);
               programAdd = 1;
               programAddSeconds = 2;
               break;
             case 2:  
               setDig("00150005", "0.000.00", 8);
-              programSeconds = toSeconds(0,15,0);
+              programSeconds = toMiliseconds(0,15,0);
               programAdd = 1;
               programAddSeconds = 5;
               break;
             case 3:  
               setDig("01300030", "0.000.00", 8);
-              programSeconds = toSeconds(1,30,0);
+              programSeconds = toMiliseconds(1,30,0);
               programAdd = 1;
               programAddSeconds = 30;
               break;
@@ -618,12 +722,29 @@ void step2(){
 
     int time1 = programSeconds;
     int time2 = programSeconds;
+
+    int player;
+
+    boolean start = false;
+    boolean started = false;
+
+    boolean led = false;
+
+    boolean delayButton = true;
+
+    int timesButton = 0;
     
 void step3(){
   while(Step3){
     while(trying){
+      lc.clearDisplay(0);
       time1 = programSeconds;
       time2 = programSeconds;
+      if(digitalRead(btnNegras) == 1){
+        player = 0;
+      }else{
+        player = 1;
+      }
       trying = false;
     }
     if(a1 == 1){
@@ -635,13 +756,79 @@ void step3(){
 }
 
 void step3_sA(){
-  toShow(time1,time2);
-  setDig("*ToShow*", "0.000.00", 8);
+  toDisplayTime(time1,time2);
+  setNum("0.000.00", 8);
+  delay(97);
+  if(start){
+    if(digitalRead(btnNegras) == 1){
+       time1 = time1 - 1;
+        if(player == 1){
+          player = 0;
+        }
+    }else{
+       time2 = time2 - 1;
+        if(player == 0){
+          player = 1;
+        }
+    }
+    if(timesButton == 0){
+      if(digitalRead(btn3) == 1){
+         start = false;
+         delayButton = true;
+      }
+    }else{
+      timesButton--;
+    }
+  }else{
+    if(delayButton){
+      delay(300);
+      delayButton = false;
+    }
+    if(digitalRead(btn3) == 1){
+       start = true;
+       started = true;
+       timesButton = 3;
+    }
+  }
 }
 
 void step3_cA(){
-  toShow(time1,time2);
-  setDig("*ToShow*", "0.000.00", 8);
+  toDisplayTime(time1,time2);
+  setNum("0.000.00", 8);
+  delay(97);
+  if(start){
+    if(digitalRead(btnNegras) == 1){
+       time1 = time1 - 1;
+        if(player == 1){
+          player = 0;
+          time1 = time1 + toMiliseconds(0,0,programAddSeconds);
+        }
+    }else{
+       time2 = time2 - 1;
+        if(player == 0){
+          player = 1;
+          time2 = time2 + toMiliseconds(0,0,programAddSeconds);
+        }
+    }
+    if(timesButton == 0){
+      if(digitalRead(btn3) == 1){
+         start = false;
+         delayButton = true;
+      }
+    }else{
+      timesButton--;
+    }
+  }else{
+    if(delayButton){
+      delay(300);
+      delayButton = false;
+    }
+    if(digitalRead(btn3) == 1){
+       start = true;
+       started = true;
+       timesButton = 3;
+    }
+  }
 }
 
   
